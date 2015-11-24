557ab8486bded20bcb66112fd00881644df60cd8
eli777tw <eli777tw@gmail.com>
2015-11-25 06:00:18 +0800

    整合FB登入
    import 了FB和相關功能的SDK
    Login有FB登入
    使用FB登入後可以到主程式
    某種未發現的未知情況下會無法用FB登入，
    不過登出還有跟一般登入模式之間的關係可能要再討論

--- AlarmClock3.0/app/src/main/java/com/trigg/alarmclock/Login.java
+++ AlarmClock3.0/app/src/main/java/com/trigg/alarmclock/Login.java
@@ -2,27 +2,65 @@ package com.trigg.alarmclock;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 
 public class Login extends Activity {
 
     EditText etUsername, etUserpass;
     String login_name, login_pass;
+    CallbackManager callbackManager;
+
+    private LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
+
+
+        callbackManager = CallbackManager.Factory.create();
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.trigg.alarmclock",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
 
         etUsername = (EditText) findViewById(R.id.user_name);
         etUserpass = (EditText) findViewById(R.id.user_pass);
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
 
         Button bToRegister = (Button) findViewById(R.id.bToRegister);
         bToRegister.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +84,8 @@ public class Login extends Activity {
             }
         });
 
+
+
         /*Button disconnect = (Button) findViewById(R.id.disconnect);
         disconnect.setOnClickListener(new View.OnClickListener() {
 
@@ -55,7 +95,52 @@ public class Login extends Activity {
 
             }
         });*/
+
+        loginButton.setReadPermissions(Arrays.asList("public_profile, email, user_birthday"));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                startActivity(new Intent(Login.this, MainActivity.class));
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i("result", "cancel");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.i("result", "error");
+            }
+        });
+
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 }
 
 
